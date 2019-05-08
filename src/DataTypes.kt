aa5d0c88979cf39a305069efb0e6a56b61bc8933
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-08 11:28:29 +0630

    Finished DataTypes.kt

---
+++ src/DataTypes.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+    var name="igm"
+    var age=21
+    var dep:String?
+    dep="UCSY"
+    println("name: "+name)
+    println("age: "+age)
+    println("dep: "+dep)
+
+    var t:Float=3.4f
+    var z:Double=3.4
+ 
+}
