cc0fe04a364aaa59c9139e429458c75aa7594ea4
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-08 14:42:34 +0630

    finished Operations.kt

---
+++ src/Operations.kt
@@ -0,0 +1,15 @@
+/*
+Math Calculator
+ */
+fun main(args: Array<String>){
+
+    println("---Math Calculator--")
+    print("enter n1: ")
+    var n1:Float=readLine()!!.toFloat()
+    print("enter n2: ")
+    var n2:Float= readLine()!!.toFloat()
+    var sum:Float?
+    sum=n1+n2
+    print("Sum: "+sum)
+
+}
