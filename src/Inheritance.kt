20cdf96e8fcd7df0d792a8bc8830920e8c574da7
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-10 10:35:27 +0630

    finished Inheritance.kt

---
+++ src/Inheritance.kt
@@ -0,0 +1,27 @@
+
+open class Operations(){
+    fun sum(n1:Int,n2:Int):Int{
+        return n1+n2
+    }
+    fun div(n1:Int,n2:Int):Int{
+        return n1/n2
+    }
+}
+
+class MultiOperations():Operations(){
+
+    fun sub(n1:Int,n2:Int):Int{
+        return n1-n2
+    }
+    fun mul(n1:Int,n2:Int):Int{
+        return n1*n2
+    }
+}
+
+fun main(args:Array<String>){
+    var op=Operations()
+    var sum=op.sum(10,15)
+    println("Sum: "+sum)
+
+
+}
