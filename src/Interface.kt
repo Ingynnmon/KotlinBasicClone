8714c1bad03d4ce15258dfdbe62d30285baf11f8
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-10 11:49:27 +0630

    modified Interface.kt

--- src/Interface.kt
+++ src/Interface.kt
@@ -1,11 +1,17 @@
-interface op{
-    fun sum(n1:Int,n2:Int)
-    fun sub(n1:Int,n2:Int)
+interface op {
+    var num: Int
+    fun sum(n1: Int, n2: Int)
+    fun sub(n1: Int, n2: Int)
 }
-
-class UserOp:op{
+class UserOp(override var num: Int):op{
     override fun sum(n1: Int, n2: Int) {
-        println("sum: "+n1+n2)
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun sub(n1: Int, n2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+fun main(args:Array<String>){
+    val userOp=UserOp(30)
 }
