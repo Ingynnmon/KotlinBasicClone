eb57c83c6357cae789d1cdc4fadaabdaae4f993f
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-13 09:47:40 +0630

    modified

--- src/MyFile.kt
+++ src/MyFile.kt
@@ -1,17 +1,61 @@
+import java.io.FileReader
 import java.io.FileWriter
 import java.lang.Exception
+import kotlin.system.exitProcess
 
 fun main(args:Array<String>){
-    print("Write to file txt: ")
-    var str:String= readLine().toString()
-    writeToFile(str)
+    println("1- read\n 2- write \n")
+    val op= readLine()!!.toInt()
+    when(op){
+        1-> {
+            ReadFromFile()
+        }
+        2-> {
+            print("Write to file txt: ")
+            var str:String= readLine().toString()
+            writeToFile(str)
+        }
+    }
+    println("Do you want to process more? y/n")
+    val choice= readLine()!!.toString()
+    while(choice.equals("y")){
+
+            println("1- read\n 2- write \n")
+            val op= readLine()!!.toInt()
+            when(op){
+                1-> {
+                    ReadFromFile()
+                }
+                2-> {
+                    print("Write to file txt: ")
+                    var str:String= readLine().toString()
+                    writeToFile(str)
+                }
+            }
+    }
+   /* */
 }
 fun writeToFile(str:String){
     try {
-        var fout = FileWriter("text.txt",true)
+        val fout = FileWriter("text.txt",true)
         fout.write(str+"\n")
         fout.close()
+        println("data is saved")
     }catch (ex:Exception){
         print(ex.message)
     }
+}
+fun ReadFromFile(){
+    try{
+        val fin=FileReader("text.txt")
+        var c:Int? //to read character
+        do{
+            c=fin.read()
+            print(c.toChar())
+        }while(c!=-1)
+
+    }catch (ex:Exception){
+        print(ex.message)
+    }
+
 }
